fcaf50cee198de6380172feae13d0a65527b8bf1
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-12 14:00:16 -0500

    Add: Button Types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,24 @@
 type ButtonProps = {
+  type?: 'default' | 'primary' | 'success' | 'danger';
   children: React.ReactNode;
 };
 
-function Button({ children }: ButtonProps) {
+function Button({ type = 'default', children }: ButtonProps) {
+  const buttonType =
+    type === 'default'
+      ? 'bg-gray-200 hover:bg-gray-300'
+      : type === 'primary'
+      ? 'bg-blue-500 hover:bg-blue-600'
+      : type === 'success'
+      ? 'bg-green-500 hover:bg-green-600'
+      : type === 'danger'
+      ? 'bg-red-500 hover:bg-red-600'
+      : 'bg-gray-200';
+
   return (
-    <button className='bg-blue-500 text-white font-medium hover:bg-blue-600 px-4 py-2 rounded-md'>
+    <button
+      className={`${buttonType} text-white font-medium px-4 py-2 rounded-md`}
+    >
       {children}
     </button>
   );
