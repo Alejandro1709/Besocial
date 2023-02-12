f3868ec9d34f2020ef45f0bacff6aca9b3a1924c
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-12 14:02:14 -0500

    Add: Dummy data to post comment

--- src/data/feed.ts
+++ src/data/feed.ts
@@ -4,7 +4,8 @@ import type IPost from '@/types/post';
 export const posts: IPost[] = [
   {
     id: '22',
-    content: '',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies tincidunt, nunc nisl ultricies nisl, nec ultricies nunc nisl vel nisl. Sed euismod, nisl vel ultricies tincidunt, nunc nisl ultricies nisl, nec ultricies nunc nisl vel nisl.',
     author: authors[0],
     likes: 4,
     comments: 2,
