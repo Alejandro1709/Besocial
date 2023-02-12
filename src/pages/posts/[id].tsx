e999d13b2d4bc6c2769f3ef38f3321a6c0a0a0f0
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-12 11:45:04 -0500

    Add: Post Page

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout';
+import Section from '@/components/Section';
+
+function PostPage() {
+  return (
+    <Layout title='Besocial | Post'>
+      <Section>
+        <h1>Post</h1>
+      </Section>
+    </Layout>
+  );
+}
+
+export default PostPage;
