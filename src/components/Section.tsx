94a839ad6364f66111c565ebb4f2c9977e326c18
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-12 14:01:35 -0500

    Add: Margin Type

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,14 +1,20 @@
 type SectionProps = {
   flex?: boolean;
   direction?: 'row' | 'col';
+  mt?: number;
   children: React.ReactNode;
 };
 
-function Section({ flex, direction, children }: SectionProps) {
+function Section({ flex, direction, mt, children }: SectionProps) {
   const isFlex = flex ? 'flex' : '';
   const dir = direction === 'row' ? 'flex-row' : 'flex-col';
+  const marginTop = mt ? `mt-${mt}` : '';
 
-  return <section className={`${isFlex} ${dir} w-full`}>{children}</section>;
+  return (
+    <section className={`${isFlex} ${dir} ${marginTop} w-full`}>
+      {children}
+    </section>
+  );
 }
 
 export default Section;
