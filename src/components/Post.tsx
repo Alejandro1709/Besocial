34c52440748e74ad00c138252081930f7af61cb1
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2023-02-14 21:09:48 -0500

    Refactor: Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,14 @@
 import NavLink from './NavLink';
 import type IPost from '@/types/post';
+import Button from './Button';
 
 type PostProps = {
   post: IPost;
+  type?: 'feed' | 'post';
 };
 
-function Post({ post }: PostProps) {
-  return (
+function Post({ post, type = 'feed' }: PostProps) {
+  return type === 'feed' ? (
     <article className='flex flex-col gap-2 p-4 bg-white rounded-md border cursor-pointer'>
       <div className='flex flex-row justify-between items-center'>
         <div className='flex flex-row gap-2 items-center'>
@@ -16,7 +18,7 @@ function Post({ post }: PostProps) {
             className='w-10 h-10 rounded-full'
             draggable='false'
           />
-          <div className=''>
+          <div className='flex flex-col'>
             <h1 className='font-medium text-gray-700'>{post.author.name}</h1>
             <p className='text-gray-500 text-sm'>2 hours ago</p>
           </div>
@@ -34,6 +36,29 @@ function Post({ post }: PostProps) {
           <button className='flex flex-row items-center ml-4'>Comment</button>
         </div>
       </div>
+    </article>
+  ) : (
+    <article className='flex flex-col gap-2 p-4 mt-4 bg-white rounded-md border cursor-pointer'>
+      <div className='flex flex-row justify-between items-center'>
+        <div className='flex flex-row gap-2 items-center'>
+          <img
+            src={post?.author.avatar}
+            alt={post?.author.name}
+            className='w-10 h-10 rounded-full'
+          />
+          <div className='flex flex-col'>
+            <h1 className='font-medium text-gray-700'>{post?.author.name}</h1>
+            <p className='text-gray-500 text-sm'>2 hours ago</p>
+          </div>
+        </div>
+        <div className='flex flex-row gap-2 items-center'>
+          <NavLink href={`/posts/${post?.id}/edit`} type='primary'>
+            Edit
+          </NavLink>
+          <Button type='danger'>Delete</Button>
+        </div>
+      </div>
+      <p className='text-gray-700'>{post?.content}</p>
     </article>
   );
 }
--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -1,8 +1,9 @@
-import Button from '@/components/Button';
+import { prisma } from '@/utils/prisma';
 import Layout from '@/components/Layout';
 import Section from '@/components/Section';
 import NavLink from '@/components/NavLink';
-import { prisma } from '@/utils/prisma';
+import Comments from '@/components/Comments';
+import Post from '@/components/Post';
 import type IPost from '@/types/post';
 
 type Props = {
@@ -18,57 +19,11 @@ function PostPage({ post }: Props) {
         <NavLink href='/' type='primary'>
           Back
         </NavLink>
-        <article className='flex flex-col gap-2 p-4 mt-4 bg-white rounded-md border cursor-pointer'>
-          <div className='flex flex-row justify-between items-center'>
-            <div className='flex flex-row gap-2 items-center'>
-              <img
-                src={parsedPost.author.avatar}
-                alt={parsedPost.author.name}
-                className='w-10 h-10 rounded-full'
-              />
-              <div className=''>
-                <h1 className='font-medium text-gray-700'>
-                  {parsedPost.author.name}
-                </h1>
-                <p className='text-gray-500 text-sm'>2 hours ago</p>
-              </div>
-            </div>
-            <div className='flex flex-row gap-2 items-center'>
-              <NavLink href={`/posts/${parsedPost.id}/edit`} type='primary'>
-                Edit
-              </NavLink>
-              <Button type='danger'>Delete</Button>
-            </div>
-          </div>
-          <p className='text-gray-700'>{parsedPost.content}</p>
-        </article>
+        <Post post={parsedPost} type='post' />
       </Section>
       <Section mt={4}>
         <h2 className='text-2xl font-medium'>1 Comment</h2>
-        <div className='flex flex-col gap-4 my-4'>
-          <article className='flex flex-col gap-2 p-4 bg-white rounded-md border cursor-pointer'>
-            <div className='flex flex-row justify-between items-center'>
-              <div className='flex flex-row gap-2 items-center'>
-                <img
-                  src='https://picsum.photos/200'
-                  alt='John Doe'
-                  className='w-10 h-10 rounded-full'
-                />
-                <div className=''>
-                  <h1 className='font-medium text-gray-700'>John Doe</h1>
-                  <p className='text-gray-500 text-sm'>2 hours ago</p>
-                </div>
-              </div>
-              <div className='flex flex-row gap-2 items-center'>
-                <Button type='primary'>Edit</Button>
-                <Button type='danger'>Delete</Button>
-              </div>
-            </div>
-            <p className='text-gray-700'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            </p>
-          </article>
-        </div>
+        <Comments />
       </Section>
     </Layout>
   );
