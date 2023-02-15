beea75bcfcf71766166ab43ddf1c45b9b6a82cf2
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2023-02-15 18:26:34 -0500

    Add: commment Component

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,30 +1,26 @@
-import Button from './Button';
+import Post from './Post';
 
 function Comments() {
+  const comment = {
+    id: '3',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam',
+    author: {
+      id: 'e3',
+      name: 'John Doe',
+      avatar: 'https://picsum.photos/200',
+      email: '',
+      password: '',
+      createdAt: '14/02/2023',
+    },
+    likes: 0,
+    comments: 0,
+    createdAt: '14/02/2023',
+  };
+
   return (
     <div className='flex flex-col gap-4 my-4'>
-      <article className='flex flex-col gap-2 p-4 bg-white rounded-md border cursor-pointer'>
-        <div className='flex flex-row justify-between items-center'>
-          <div className='flex flex-row gap-2 items-center'>
-            <img
-              src='https://picsum.photos/200'
-              alt='John Doe'
-              className='w-10 h-10 rounded-full'
-            />
-            <div className=''>
-              <h1 className='font-medium text-gray-700'>John Doe</h1>
-              <p className='text-gray-500 text-sm'>2 hours ago</p>
-            </div>
-          </div>
-          <div className='flex flex-row gap-2 items-center'>
-            <Button type='primary'>Edit</Button>
-            <Button type='danger'>Delete</Button>
-          </div>
-        </div>
-        <p className='text-gray-700'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        </p>
-      </article>
+      <Post post={comment} type='post' />
     </div>
   );
 }
